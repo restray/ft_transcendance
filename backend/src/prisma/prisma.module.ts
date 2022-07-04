b433ae2c70fcfaf7a7413c7cf9d47403846a4c99
Restray <contact@restray.org>
2022-07-04 19:57:23 +0200

    feat(doc): add api

--- backend/src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-import { UserService } from './user/user.service';
-
-@Module({
-  providers: [PrismaService, UserService],
-  exports: [UserService],
-})
-export class PrismaModule {}
