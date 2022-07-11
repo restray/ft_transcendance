6629e84c00e472c42fdfd25361015c28a191dcfa
Restray <contact@restray.org>
2022-07-11 14:54:06 +0200

    Update examples

--- index.js
+++ index.js
@@ -28,31 +28,41 @@ handleFriendsStatus = (BearerToken) => {
 handleChannelsMessages = (BearerToken) => {
     const channels = io("ws://localhost:3000/channels", { auth: { token: BearerToken }})
     
-    channels.on('connect_error', err => handleErrors(err))
-    channels.on('connect_failed', err => handleErrors(err))
-
     channels.on("connect", () => {
         console.info("Socket channels status is connected!")
     });
+    channels.on('connect_error', err => handleErrors(err))
+    channels.on('connect_failed', err => handleErrors(err))
 
+    // A message has been sent on a channel where i am
     channels.on('message', (info) => {
         console.log("message", info)
     })
 
+    // An user has join a channel where i am
     channels.on('join', (info) => {
         console.log("join", info)
     })
+    // An user leave join a channel where i am
     channels.on('leave', (info) => {
         console.log("leave", info)
     })
+    // An user has perform an action in a channel where i am
     channels.on('action', (info) => {
         console.log("action", info)
     })
 
+    // Receive dm message
+    channels.on('dm_message', (info) => {
+        console.log(info);
+    })
+
+    // Any Exception happening on "channels" socket
     channels.on('exception', (error) => {
         console.log(error)
     })
-
+    
+    // Send a message
     document.getElementById("sendMessage").addEventListener("click", () => {
         channels.emit("message", {
             channel: document.getElementById("channel").value,
@@ -62,6 +72,7 @@ handleChannelsMessages = (BearerToken) => {
         });
     })
     
+    // Join a channel
     document.getElementById("joinChannel").addEventListener("click", () => {
         const headers = new Headers();
         headers.append('Authorization', `Bearer ${BearerToken}`)
@@ -76,6 +87,7 @@ handleChannelsMessages = (BearerToken) => {
         })
     })
     
+    // Leave a channel
     document.getElementById("leaveChannel").addEventListener("click", () => {
         const headers = new Headers();
         headers.append('Authorization', `Bearer ${BearerToken}`)
@@ -89,6 +101,32 @@ handleChannelsMessages = (BearerToken) => {
             console.error(error);
         })
     })
+
+    // Create a DM Channel
+    document.getElementById("createDmChannel").addEventListener("click", () => {
+        const headers = new Headers();
+        headers.append('Authorization', `Bearer ${BearerToken}`)
+
+        fetch(`http://localhost:3000/dm/${document.getElementById("dmChannel").value}/`, {
+            method: "POST",
+            headers: headers
+        }).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.error(error);
+        })
+    })
+    
+    document.getElementById("sendDmMessage").addEventListener("click", () => {
+        channels.emit("dm_message", {
+            user: document.getElementById("dmChannel").value,
+            message: document.getElementById("dmMessage").value
+        }, (response) => {
+            console.log(response); // ok
+        });
+    })
+    
+    
 }
 
 document.getElementById('login').addEventListener("click", (event) => {
