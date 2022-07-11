398ca34b8085195375d0cd90fa0a29c525061a32
Restray <contact@restray.org>
2022-07-11 10:58:45 +0200

    patch #3 (channels): add messages on message event

--- backend/src/prisma/channels/channels.service.ts
+++ backend/src/prisma/channels/channels.service.ts
@@ -39,6 +39,11 @@ export class ChannelsService {
             },
           },
         },
+        messages: {
+          include: {
+            User: true,
+          },
+        },
       },
     });
   }
